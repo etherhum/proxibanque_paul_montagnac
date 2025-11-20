c3a538c261a8d4d6a5ad47eafa1b9a70726dd238
Paul Montagnac <paul.montagnac@epita.fr>
2025-11-20 10:33:17 +0100

    init

---
+++ src/main/java/fr/epita/projet_paul_montagnac/ProjetPaulMontagnacApplication.java
@@ -0,0 +1,13 @@
+package fr.epita.projet_paul_montagnac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPaulMontagnacApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetPaulMontagnacApplication.class, args);
+  }
+
+}
---
+++ src/test/java/fr/epita/projet_paul_montagnac/ProjetPaulMontagnacApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.projet_paul_montagnac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPaulMontagnacApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
